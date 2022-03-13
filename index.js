06db86bdc5a4508fad8e66812c63132f91ff3f14
saminakhan999 <samina.khan9999@gmail.com>
2022-03-13 01:07:07 +0000

    defined all params

--- index.js
+++ index.js
@@ -3,3 +3,32 @@ function setup() {
   textAlign(CENTER, CENTER);
   frameRate(60);
 }
+
+var level = 1;
+var platforms = [];
+var ice = [];
+var cannons = [];
+var bullets = [];
+var tramps = [];
+var spikes = [];
+var keys = [];
+var gravity = 0.8;
+var offGround = 10;
+var timeSinceJump = 1000;
+var framesSinceStart;
+var Player = {
+  x: 400,
+  y: 400,
+  w: 30,
+  h: 30,
+  ySpeed: 0,
+  xSpeed: 0,
+  health: 150,
+};
+var Portal = {
+  x: 0,
+  y: 0,
+  r: 0,
+  time: 0,
+};
+
